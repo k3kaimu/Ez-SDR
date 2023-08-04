ac17ee47a193c56c4996a2985d3844415091a581
Komatsu Kazuki <komatsu.kazuki.op@tut.jp>
2023-08-04 10:45:03 +0900

    update compress and decompress

--- client/sigdatafmt.py
+++ client/sigdatafmt.py
@@ -61,21 +61,36 @@ def writeFloat32ToSock(sock, value):
     sock.sendall(data)
 
 
+def getMinStep(rsignal):
+    rsignal = np.abs(rsignal)
+    return np.min(rsignal[rsignal != 0])
+
 def compress(signal):
+    scale = 32767
+    minStep = getMinStep(np.real(signal[:min(10000000, len(signal))]))
+
+    # get min
+    if minStep < (1/32766) and minStep > (1/32768):
+        scale = 1/minStep
+
     data = np.hstack((np.real(signal), np.imag(signal))).astype(np.float32)
-    data *= 32767
+    data *= scale
     data = data.astype(np.int16)
     data += 2**7
     data = data.tobytes()
-    data = np.hstack((data[::2], data[1::2]))
+    header = np.array([scale], dtype=np.float32).tobytes()
+    data = header + data[::2] + data[1::2]
     return zlib.compress(data)
 
 
 def decompress(data):
     data = zlib.decompress(data)
-    ldata = np.frombuffer(data[0:len(data)//2], np.uint8)
-    hdata = np.frombuffer(data[len(data)//2:len(data)], np.uint8)
-    data = np.frombuffer(np.concatenate(np.vstack((ldata, hdata)).T).tobytes(), dtype=np.int16)
+    scale = np.frombuffer(data[:4], np.float32)[0]
+    data = data[4:]
+    bdata = bytearray(len(data))
+    bdata[0::2] = data[:len(data)//2]
+    bdata[1::2] = data[len(data)//2:]
+    data = np.frombuffer(bdata, dtype=np.int16)
     data = data - 2**7
-    data = data.astype(np.float32) / 32767
+    data = data.astype(np.float32) / scale
     return data[:len(data)//2] + data[len(data)//2:] * 1j
