acbb4ca861bf6f73de299c75210d5fe232590978
Kazuki Komatsu <komatsu.kazuki.op@tut.jp>
2024-07-07 11:18:27 +0900

    update dub.json

--- cpp/looptx_rfnoc_replay_block.cpp
+++ cpp/looptx_rfnoc_replay_block.cpp
@@ -81,132 +81,132 @@ DeviceHandler* setupDevice(
     }
     auto replay_ctrl = graph->get_block<uhd::rfnoc::replay_block_control>(replay_ctrl_id);
 
-    // Connect replay to radio
-    auto edges = uhd::rfnoc::connect_through_blocks(graph, replay_ctrl_id, replay_chan, radio_ctrl_id, radio_chan);
+    // // Connect replay to radio
+    // auto edges = uhd::rfnoc::connect_through_blocks(graph, replay_ctrl_id, replay_chan, radio_ctrl_id, radio_chan);
 
-    // Check for a DUC connected to the radio
-    uhd::rfnoc::duc_block_control::sptr duc_ctrl;
-    size_t duc_chan = 0;
-    for (auto& edge : edges) {
-        auto blockid = uhd::rfnoc::block_id_t(edge.dst_blockid);
-        if (blockid.match("DUC")) {
-            duc_ctrl = graph->get_block<uhd::rfnoc::duc_block_control>(blockid);
-            duc_chan = edge.dst_port;
-            break;
-        }
-    }
+    // // Check for a DUC connected to the radio
+    // uhd::rfnoc::duc_block_control::sptr duc_ctrl;
+    // size_t duc_chan = 0;
+    // for (auto& edge : edges) {
+    //     auto blockid = uhd::rfnoc::block_id_t(edge.dst_blockid);
+    //     if (blockid.match("DUC")) {
+    //         duc_ctrl = graph->get_block<uhd::rfnoc::duc_block_control>(blockid);
+    //         duc_chan = edge.dst_port;
+    //         break;
+    //     }
+    // }
 
-    // Report blocks
-    std::cout << "Using Radio Block:  " << radio_ctrl_id << ", channel " << radio_chan
-              << std::endl;
-    std::cout << "Using Replay Block: " << replay_ctrl_id << ", channel " << replay_chan
-              << std::endl;
-    if (duc_ctrl) {
-        std::cout << "Using DUC Block:    " << duc_ctrl->get_block_id() << ", channel "
-                  << duc_chan << std::endl;
-    }
-
-
-    /************************************************************************
-     * Set up streamer to Replay block and commit graph
-     ***********************************************************************/
-    uhd::device_addr_t streamer_args;
-    uhd::stream_args_t stream_args(cpu_format, wire_format);
-    uhd::tx_streamer::sptr tx_stream;
-    uhd::tx_metadata_t tx_md;
-
-    stream_args.args = streamer_args;
-    tx_stream        = graph->create_tx_streamer(1, stream_args);
-    graph->connect(tx_stream, 0, replay_ctrl->get_block_id(), replay_chan);
-    graph->commit();
-    dev->streamer = tx_stream;
-
-    /************************************************************************
-     * Set up radio
-     ***********************************************************************/
-    // Set clock reference
-    if (clockref.size() > 0) {
-        // Lock mboard clocks
-        for (size_t i = 0; i < graph->get_num_mboards(); ++i) {
-            graph->get_mb_controller(i)->set_clock_source(clockref);
-        }
-    }
+    // // Report blocks
+    // std::cout << "Using Radio Block:  " << radio_ctrl_id << ", channel " << radio_chan
+    //           << std::endl;
+    // std::cout << "Using Replay Block: " << replay_ctrl_id << ", channel " << replay_chan
+    //           << std::endl;
+    // if (duc_ctrl) {
+    //     std::cout << "Using DUC Block:    " << duc_ctrl->get_block_id() << ", channel "
+    //               << duc_chan << std::endl;
+    // }
 
 
-    if(timeref.size() > 0) {
-        // set time source
-        for (size_t i = 0; i < graph->get_num_mboards(); ++i) {
-            graph->get_mb_controller(i)->set_time_source(timeref);
-        }
-    }
+    // /************************************************************************
+    //  * Set up streamer to Replay block and commit graph
+    //  ***********************************************************************/
+    // uhd::device_addr_t streamer_args;
+    // uhd::stream_args_t stream_args(cpu_format, wire_format);
+    // uhd::tx_streamer::sptr tx_stream;
+    // uhd::tx_metadata_t tx_md;
 
-    // Apply any radio arguments provided
-    if (tx_args.size() > 0) {
-        radio_ctrl->set_tx_tune_args(tx_args, radio_chan);
-    }
+    // stream_args.args = streamer_args;
+    // tx_stream        = graph->create_tx_streamer(1, stream_args);
+    // graph->connect(tx_stream, 0, replay_ctrl->get_block_id(), replay_chan);
+    // graph->commit();
+    // dev->streamer = tx_stream;
 
-    // Set the center frequency
-    if (freq < 0) {
-        std::cerr << "Please specify the center frequency with 'freq'" << std::endl;
-        return nullptr;
-    }
+    // /************************************************************************
+    //  * Set up radio
+    //  ***********************************************************************/
+    // // Set clock reference
+    // if (clockref.size() > 0) {
+    //     // Lock mboard clocks
+    //     for (size_t i = 0; i < graph->get_num_mboards(); ++i) {
+    //         graph->get_mb_controller(i)->set_clock_source(clockref);
+    //     }
+    // }
 
-    std::cout << std::fixed;
-    std::cout << "Requesting TX Freq: " << (freq / 1e6) << " MHz..." << std::endl;
-    radio_ctrl->set_tx_frequency(freq, radio_chan);
-    std::cout << "Actual TX Freq: " << (radio_ctrl->get_tx_frequency(radio_chan) / 1e6)
-              << " MHz..." << std::endl
-              << std::endl;
-    std::cout << std::resetiosflags(std::ios::fixed);
 
-    // Set the sample rate
-    if (rate >= 0) {
-        std::cout << std::fixed;
-        std::cout << "Requesting TX Rate: " << (rate / 1e6) << " Msps..." << std::endl;
-        if (duc_ctrl) {
-            std::cout << "DUC block found." << std::endl;
-            duc_ctrl->set_input_rate(rate, duc_chan);
-            std::cout << "  Interpolation value is "
-                      << duc_ctrl->get_property<int>("interp", duc_chan) << std::endl;
-            rate = duc_ctrl->get_input_rate(duc_chan);
-        } else {
-            rate = radio_ctrl->set_rate(rate);
-        }
-        std::cout << "Actual TX Rate: " << (rate / 1e6) << " Msps..." << std::endl
-                  << std::endl;
-        std::cout << std::resetiosflags(std::ios::fixed);
-    }
+    // if(timeref.size() > 0) {
+    //     // set time source
+    //     for (size_t i = 0; i < graph->get_num_mboards(); ++i) {
+    //         graph->get_mb_controller(i)->set_time_source(timeref);
+    //     }
+    // }
 
-    // Set the RF gain
-    if (gain >= 0) {
-        std::cout << std::fixed;
-        std::cout << "Requesting TX Gain: " << gain << " dB..." << std::endl;
-        radio_ctrl->set_tx_gain(gain, radio_chan);
-        std::cout << "Actual TX Gain: " << radio_ctrl->get_tx_gain(radio_chan) << " dB..."
-                  << std::endl
-                  << std::endl;
-        std::cout << std::resetiosflags(std::ios::fixed);
-    }
+    // // Apply any radio arguments provided
+    // if (tx_args.size() > 0) {
+    //     radio_ctrl->set_tx_tune_args(tx_args, radio_chan);
+    // }
 
-    // Set the analog front-end filter bandwidth
-    if (bw >= 0) {
-        std::cout << std::fixed;
-        std::cout << "Requesting TX Bandwidth: " << (bw / 1e6) << " MHz..." << std::endl;
-        radio_ctrl->set_tx_bandwidth(bw, radio_chan);
-        std::cout << "Actual TX Bandwidth: "
-                  << (radio_ctrl->get_tx_bandwidth(radio_chan) / 1e6) << " MHz..."
-                  << std::endl
-                  << std::endl;
-        std::cout << std::resetiosflags(std::ios::fixed);
-    }
+    // // Set the center frequency
+    // if (freq < 0) {
+    //     std::cerr << "Please specify the center frequency with 'freq'" << std::endl;
+    //     return nullptr;
+    // }
 
-    // Set the antenna
-    if (ant.size() > 0) {
-        radio_ctrl->set_tx_antenna(ant, radio_chan);
-    }
+    // std::cout << std::fixed;
+    // std::cout << "Requesting TX Freq: " << (freq / 1e6) << " MHz..." << std::endl;
+    // radio_ctrl->set_tx_frequency(freq, radio_chan);
+    // std::cout << "Actual TX Freq: " << (radio_ctrl->get_tx_frequency(radio_chan) / 1e6)
+    //           << " MHz..." << std::endl
+    //           << std::endl;
+    // std::cout << std::resetiosflags(std::ios::fixed);
 
-    // Allow for some setup time
-    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    // // Set the sample rate
+    // if (rate >= 0) {
+    //     std::cout << std::fixed;
+    //     std::cout << "Requesting TX Rate: " << (rate / 1e6) << " Msps..." << std::endl;
+    //     if (duc_ctrl) {
+    //         std::cout << "DUC block found." << std::endl;
+    //         duc_ctrl->set_input_rate(rate, duc_chan);
+    //         std::cout << "  Interpolation value is "
+    //                   << duc_ctrl->get_property<int>("interp", duc_chan) << std::endl;
+    //         rate = duc_ctrl->get_input_rate(duc_chan);
+    //     } else {
+    //         rate = radio_ctrl->set_rate(rate);
+    //     }
+    //     std::cout << "Actual TX Rate: " << (rate / 1e6) << " Msps..." << std::endl
+    //               << std::endl;
+    //     std::cout << std::resetiosflags(std::ios::fixed);
+    // }
+
+    // // Set the RF gain
+    // if (gain >= 0) {
+    //     std::cout << std::fixed;
+    //     std::cout << "Requesting TX Gain: " << gain << " dB..." << std::endl;
+    //     radio_ctrl->set_tx_gain(gain, radio_chan);
+    //     std::cout << "Actual TX Gain: " << radio_ctrl->get_tx_gain(radio_chan) << " dB..."
+    //               << std::endl
+    //               << std::endl;
+    //     std::cout << std::resetiosflags(std::ios::fixed);
+    // }
+
+    // // Set the analog front-end filter bandwidth
+    // if (bw >= 0) {
+    //     std::cout << std::fixed;
+    //     std::cout << "Requesting TX Bandwidth: " << (bw / 1e6) << " MHz..." << std::endl;
+    //     radio_ctrl->set_tx_bandwidth(bw, radio_chan);
+    //     std::cout << "Actual TX Bandwidth: "
+    //               << (radio_ctrl->get_tx_bandwidth(radio_chan) / 1e6) << " MHz..."
+    //               << std::endl
+    //               << std::endl;
+    //     std::cout << std::resetiosflags(std::ios::fixed);
+    // }
+
+    // // Set the antenna
+    // if (ant.size() > 0) {
+    //     radio_ctrl->set_tx_antenna(ant, radio_chan);
+    // }
+
+    // // Allow for some setup time
+    // std::this_thread::sleep_for(std::chrono::milliseconds(200));
 
     return reinterpret_cast<DeviceHandler*>(dev);
 }
@@ -243,8 +243,8 @@ void test1()
 }
 
 
-int main()
-{
-    looptx_rfnoc_replay_block::test1();
-    return 0;
-}
+// int main()
+// {
+//     looptx_rfnoc_replay_block::test1();
+//     return 0;
+// }
