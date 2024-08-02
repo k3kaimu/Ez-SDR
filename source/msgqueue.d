9047275068820d0db93815293a7ca62a7b773178
Komatsu Kazuki (usrp-lnx01) <komatsu.kazuki.op@tut.jp>
2024-08-02 17:11:20 +0900

    add NotifiedLazy and RequestQueue

--- source/msgqueue.d
+++ source/msgqueue.d
@@ -7,6 +7,252 @@ import std.experimental.allocator;
 import automem.vector;
 
 
+
+/** 単一スレッドからの書き込みと，単一スレッドからの読み込みを許す通知付きオブジェクト．
+一度書き込みをすると，それ以降は読み取り専用となる．
+*/
+struct NotifiedLazy(T)
+{
+    import core.sync.event;
+    alias alloc = Mallocator.instance;
+
+    void initialize()
+    {
+        _response = cast(shared)alloc.make!T;
+        _nofity = alloc.make!Event;
+        _isNull = cast(shared)alloc.make!bool;
+        *_isNull = true;
+        _nofity.initialize(true, false);
+    }
+
+
+    void terminate()
+    {
+        _nofity.terminate();
+
+        Mallocator.instance.dispose(cast()_response);
+        _response = null;
+        Mallocator.instance.dispose(cast()_nofity);
+        _nofity = null;
+        Mallocator.instance.dispose(cast()_isNull);
+        _isNull = null;
+    }
+
+
+    void write(T value)
+    {
+        // 一度書き込みをすると，それ以降は書き込めない
+        if(!*_isNull) return;
+        *_isNull = false;
+        *cast()_response = value;
+        _nofity.setIfInitialized();
+    }
+
+
+    ref T read()
+    {
+        // 書き込みされるまで読み込めない
+        _nofity.wait();
+        assert(!*_isNull);
+        return *cast(T*)_response;
+    }
+
+  private:
+    shared(T)* _response;
+    Event* _nofity;
+    shared(bool)* _isNull;
+}
+
+unittest
+{
+    NotifiedLazy!int msg;
+    msg.initialize();
+    scope(exit) msg.terminate();
+
+    assert(*(msg._isNull) == true);
+
+    msg.write(1);
+    assert(*(msg._response) == 1);
+    assert(*(msg._isNull) == false);
+    msg.write(2);
+    assert(*(msg._response) == 1);
+    assert(*(msg._isNull) == false);
+    assert(msg.read == 1);
+}
+
+unittest
+{
+    import core.thread;
+
+    NotifiedLazy!int msg1, msg2;
+    msg1.initialize();
+    msg2.initialize();
+    scope(exit) {
+        msg1.terminate();
+        msg2.terminate();
+    }
+
+    auto thread = new Thread((){
+        assert(msg2.read == 2);
+        assert(msg1.read == 1);
+    }).start();
+    scope(exit) thread.join();
+
+    msg1.write(1);
+    msg2.write(2);
+}
+
+
+final class RequestQueue(Req, Flag!"assumeUnique" assumeUnique = No.assumeUnique,  Allocator = Mallocator)
+{
+    import core.lifetime : move;
+
+    static if(assumeUnique)
+    {
+        private {
+            alias IOReq = Req;
+        }
+    }
+    else
+    {
+        private {
+            alias IOReq = shared(Req);
+        }
+    }
+
+    static if(!is(typeof(Allocator.instance)))
+    {
+        this(Allocator alloc)
+        {
+            _alloc = alloc;
+        }
+
+
+        ref Allocator allocator() {
+            return _alloc;
+        }
+
+
+        private Allocator _alloc;
+    }
+    else
+    {
+        static ref auto allocator() {
+            return Allocator.instance;
+        }
+    }
+
+
+    void pushRequest()(auto ref IOReq req) shared
+    {
+        IOReq* p = this.allocator.make!(IOReq)();
+        move(req, *p);
+        _reqList.push(cast(shared) p);
+    }
+
+
+    IOReq popRequest() shared
+    {
+        IOReq* p = cast(IOReq*)_reqList.pop();
+        scope(exit) this.allocator.dispose(p);
+        return move(*p);
+    }
+
+
+    bool emptyRequest() shared
+    {
+        return _reqList.empty;
+    }
+
+
+    Commander makeCommander() shared
+    {
+        return new Commander(this);
+    }
+
+
+    Executer makeExecuter() shared
+    {
+        return new Executer(this);
+    }
+
+
+    static final class Commander
+    {
+        this(shared RequestQueue queue)
+        {
+            _queue = queue;
+        }
+
+
+        void push(Req req)
+        {
+            _queue.pushRequest(req);
+        }
+
+
+        shared RequestQueue _queue;
+    }
+
+
+    static final class Executer
+    {
+        this(shared RequestQueue queue)
+        {
+            _queue = queue;
+        }
+
+
+        IOReq[] allRequestList()
+        {
+            return _reqList[];
+        }
+
+
+        void peekAllRequest()
+        {
+            while(! _queue.emptyRequest)
+                _reqList ~= _queue.popRequest();
+        }
+
+
+        bool empty()
+        {
+            this.peekAllRequest();
+            return _reqList.empty;
+        }
+
+
+        IOReq front()
+        {
+            this.peekAllRequest();
+            return _reqList[0];
+        }
+
+
+        void popFront()
+        {
+            this.peekAllRequest();
+            _reqList.popFront();
+        }
+
+
+      private:
+        shared RequestQueue _queue;
+        Vector!(IOReq, Allocator) _reqList;
+    }
+
+
+
+  private:
+    RWQueue!(Req*) _reqList;
+}
+
+
+alias UniqueRequestQueue(Req, Allocator = Mallocator) = RequestQueue!(Req, Yes.assumeUnique, Allocator);
+
+
+
 /**
 Move Semantics Message Queue
 */
@@ -242,3 +488,6 @@ alias UniqueMsgQueue(Req, Res, Allocator = Mallocator) = MsgQueue!(Req, Res, Yes
 
 //     UniqueMsgQueue!(S, U) q;
 // }
+
+
+
