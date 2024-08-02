67000eb00032fd2cbdf578f0eb55911a5ce0a429
Komatsu Kazuki (usrp-lnx01) <komatsu.kazuki.op@tut.jp>
2024-08-02 17:12:08 +0900

    update loop controller

--- source/controller/looptx.d
+++ source/controller/looptx.d
@@ -1,6 +1,7 @@
 module controller.looptx;
 
 import core.thread;
+import core.sync.event;
 
 import std.json;
 import std.sumtype;
@@ -19,60 +20,200 @@ import utils;
 
 
 
-class LoopTXController(C) : IController
+class LoopTXControllerThread(C) : ControllerThreadImpl!(ILoopTransmitter!C)
 {
-    enum size_t maxTotalStream = 32;
+    alias alloc = Mallocator.instance;
 
-    this() {}
 
-    void setup(IDevice[] devs, JSONValue[string])
+    this()
+    {
+        super();
+        _queue = new Requests.Queue;
+    }
+
+
+    override
+    void onInit()
+    {
+        isStreaming = false;
+        isPaused = false;
+    }
+
+
+    override
+    void onStart() { }
+
+
+    override
+    void onRunTick()
+    {
+        // writeln("OnRunTick");
+        while(!_queue.emptyRequest()) {
+            auto req = _queue.popRequest();
+
+            req.match!(
+                (Requests.SetTransmitSignal r) {
+                    size_t idx = 0;
+                    foreach(d; this.deviceList) {
+                        d.setLoopTransmitSignal(r.buffer[idx .. idx + d.numTxStream]);
+                        idx += d.numTxStream;
+                    }
+
+                    foreach(ref e; r.buffer) {
+                        alloc.dispose(e);
+                        e = null;
+                    }
+                    alloc.dispose(r.buffer);
+                    r.buffer = null;
+                },
+                (Requests.StartLoopTransmit) {
+                    foreach(d; this.deviceList) {
+                        d.startLoopTransmit();
+                    }
+                    isStreaming = true;
+                },
+                (Requests.StopLoopTransmit) {
+                    foreach(d; this.deviceList) {
+                        d.stopLoopTransmit();
+                    }
+                    isStreaming = false;
+                }
+            )();
+        }
+
+        // writeln("OnRunTick2: ", isStreaming);
+
+        if(isStreaming) {
+            foreach(d; this.deviceList)
+                d.performLoopTransmit();
+        }
+    }
+
+
+    override
+    void onFinish()
+    {
+        if(isStreaming) {
+            foreach(d; this.deviceList)
+                d.stopLoopTransmit();
+        }
+
+        isStreaming = false;
+    }
+
+
+    override
+    void onPause()
+    {
+        if(isStreaming) {
+            foreach(d; this.deviceList)
+                d.stopLoopTransmit();
+
+            isStreaming = false;
+            isPaused = true;
+        }
+    }
+
+
+    override
+    void onResume()
+    {
+        if(isPaused) {
+            foreach(d; this.deviceList)
+                d.startLoopTransmit();
+
+            isStreaming = true;
+            isPaused = false;
+        }
+    }
+
+
+    void setTransmitSignal(scope const(C)[][] signals)
+    {
+        Requests.SetTransmitSignal req;
+        req.buffer = alloc.makeArray!(C[])(signals.length);
+        foreach(i; 0 .. signals.length) {
+            req.buffer[i] = alloc.makeArray!C(signals[i].length);
+            req.buffer[i][] = signals[i][];
+        }
+
+        _queue.pushRequest(Requests.Types(req));
+    }
+
+
+    void startLoopTransmit()
+    {
+        _queue.pushRequest(Requests.Types(Requests.StartLoopTransmit()));
+    }
+
+
+    void stopLoopTransmit()
+    {
+        _queue.pushRequest(Requests.Types(Requests.StopLoopTransmit()));
+    }
+
+
+  private:
+    bool isStreaming = false;
+    bool isPaused = false;
+    shared(Requests.Queue) _queue;
+
+
+    static struct Requests
+    {
+        static struct SetTransmitSignal { C[][] buffer; }
+        static struct StartLoopTransmit {}
+        static struct StopLoopTransmit {}
+
+        alias Types = SumType!(SetTransmitSignal, StartLoopTransmit, StopLoopTransmit);
+        alias Queue = UniqueRequestQueue!Types;
+    }
+}
+
+
+class LoopTXController(C) : ControllerImpl!(LoopTXControllerThread!C)
+{
+    this()
+    {
+        super();
+    }
+
+
+    override
+    void setup(IDevice[] devs, JSONValue[string] settings)
     {
         foreach(i, e; devs) {
             _devs ~= enforce(cast(ILoopTransmitter!C) e, "The device#%s is not a ILoopTransmitter.".format(i));
-            _msgQueueList ~= new UniqueMsgQueue!(ReqTypes, ResTypes)();
-
-            enforce(e.numTxStream() <= maxTotalStream, "The controller '%s' can handle only %s streams for each device at most.".format(typeof(this).stringof, maxTotalStream));
         }
 
-        this._killSwitch = false;
+        if("singleThread" in settings && settings["singleThread"].get!bool)
+            _singleThread = true;
     }
-    
 
+
+    override
     void spawnDeviceThreads()
     {
-        auto makeThread(alias fn, T...)(ref shared(bool) stop_signal_called, T args)
-        {
-            return new Thread(delegate(){
-                scope(exit) stop_signal_called = true;
+        if(_singleThread) {
+            auto thread = new LoopTXControllerThread!C();
+            foreach(d; _devs)
+                thread.registerDevice(d);
 
-                try
-                    fn(stop_signal_called, args);
-                catch(Throwable ex){
-                    import std.stdio;
-                    writeln(ex);
-                }
-            });
-        }
+            this.registerThread(thread);
+            thread.start();
+        } else {
+            foreach(d; _devs) {
+                auto thread = new LoopTXControllerThread!C();
+                thread.registerDevice(d);
 
-        foreach(i, d; _devs) {
-            _devthreads ~= makeThread!(loopTXControllerDeviceThread!C)(this._killSwitch, d, this._msgQueueList[i].makeExecuter);
-            _devthreads[i].start();
+                this.registerThread(thread);
+                thread.start();
+            }
         }
     }
 
 
-    void killDeviceThreads()
-    {
-        _killSwitch = true;
-    }
-
-
-    // 先頭1バイトでメッセージの種類を判断
-    // 0x000XXXXX   デバイスセット設定など（32種類）
-    // 0b001XXXXX   送信系（32種類）
-    // 0x010XXXXX   受信系（32種類）
-    // 0b011XXXXX   
-    // 0x1XXXXXXX　ユーザー定義命令
+    override
     void processMessage(scope const(ubyte)[] msgbin, void delegate(scope const(ubyte)[]) writer)
     {
         auto alloc = Mallocator.instance;
@@ -84,34 +225,53 @@ class LoopTXController(C) : IController
             return;
 
         switch(reader.read!ubyte) {
-        case 0b0010000:     // 送信信号の設定
+        case 0b00001000:        // Resume Device Thread
+            this.resumeDeviceThreads();
+            break;
+
+        case 0b00001001:
+            this.pauseDeviceThreads();
+            break;
+
+        case 0b00010000:        // 送信信号の設定
             size_t cntStream = 0;
-            foreach(i, d; _devs) {
-                RequestTypes!C.SetTransmitSignal req;
-                foreach(j; 0 .. d.numTxStream()) {
-                    if(!reader.canRead!size_t) { dbg.writefln("Cannot read %s-th signal length", cntStream); return; }
-                    immutable siglen = reader.read!size_t;
-                    dbg.writefln("siglen = %s", siglen);
+            const(C)[] parseSignal() {
+                if(!reader.canRead!size_t) { dbg.writefln("Cannot read %s-th signal length", cntStream); return null; }
+                immutable siglen = reader.read!size_t;
+                dbg.writefln("siglen = %s", siglen);
 
-                    if(!reader.canReadArray!C(siglen)) { dbg.writefln("Cannot read %s-th signal (len = %s)", cntStream, siglen); return; }
-                    const(C)[] sig = reader.readArray!C(siglen);
-                    ++cntStream;
+                if(!reader.canReadArray!C(siglen)) { dbg.writefln("Cannot read %s-th signal (len = %s)", cntStream, siglen); return null; }
+                return reader.readArray!C(siglen);
+            }
 
-                    C[] buf = alloc.makeArray!C(siglen);
-                    buf[] = sig[];
-                    req.buffer[j] = buf;
+            size_t totStream = 0;
+            foreach(d; _devs) totStream += d.numTxStream();
+            if(_singleThread) {
+                const(C)[][] buffer = alloc.makeArray!(const(C)[])(totStream);
+                scope(exit) alloc.dispose(buffer);
+
+                foreach(i; 0 .. totStream) buffer[i] = parseSignal();
+                this.threadList[0].setTransmitSignal(buffer);
+            } else {
+                foreach(i, t; this.threadList) {
+                    assert(t.deviceList.length == 1);
+                    auto d = t.deviceList[0];
+                    const(C)[][] buffer = alloc.makeArray!(const(C)[])(d.numTxStream);
+                    scope(exit) alloc.dispose(buffer);
+
+                    foreach(j; 0 .. d.numTxStream) buffer[j] = parseSignal();   
+                    t.setTransmitSignal(buffer);
                 }
-                _msgQueueList[i].pushRequest(ReqTypes(req));
             }
             break;
 
-        case 0b0010001:     // ループ送信の開始
-            foreach(d; _devs)
-                d.startLoopTransmit();
+        case 0b00010001:     // ループ送信の開始
+            foreach(t; this.threadList)
+                t.startLoopTransmit();
             break;
-        case 0b0010010:     // ループ送信の終了
-            foreach(d; _devs)
-                d.stopLoopTransmit();
+        case 0b00010010:     // ループ送信の終了
+            foreach(t; this.threadList)
+                t.stopLoopTransmit();
             break;
         default:
             dbg.writefln("Unsupported msgtype %X", msgbin[0]);
@@ -121,106 +281,9 @@ class LoopTXController(C) : IController
 
   private:
     ILoopTransmitter!C[] _devs;
-    shared(bool) _killSwitch;
-    shared(UniqueMsgQueue!(ReqTypes, ResTypes))[] _msgQueueList;
-    Thread[] _devthreads;
-
-
-    alias ReqTypes = SumType!(
-        RequestTypes!C.SetTransmitSignal,
-        RequestTypes!C.StartLoopTransmit,
-        RequestTypes!C.StopLoopTransmit,
-        RequestTypes!C.SetParam,
-        RequestTypes!C.SyncPPS);
-
-    alias ResTypes = SumType!(int);
-
-    static struct RequestTypes(C)
-    {
-        static struct SetTransmitSignal
-        {
-            C[][maxTotalStream] buffer;
-        }
-
-
-        static struct StartLoopTransmit {}
-        static struct StopLoopTransmit {}
-
-        static struct SetParam
-        {
-            string key;
-            string value;
-        }
-
-        static struct SyncPPS
-        {
-            DeviceTime settling;
-        }
-    }
-
-
-    static struct ResponseTypes(C)
-    {
-
-    }
-
-
-    static void loopTXControllerDeviceThread(C)(
-        ref shared(bool) killSwitch,
-        ILoopTransmitter!C dev,
-        UniqueMsgQueue!(ReqTypes, ResTypes).Executer msgQueue)
-    {
-        alias dbg = debugMsg!"loopTXControllerDeviceThread";
-        auto alloc = Mallocator.instance;
-
-        immutable size_t numTxStream = dev.numTxStream;
-        bool isStreaming = false;
-
-        while(!killSwitch) {
-            // すべてのリクエストを処理する
-            while(! msgQueue.emptyRequest) {
-                dbg.writeln("POP Request");
-
-                auto req = cast()msgQueue.popRequest();
-                req.match!(
-                    (RequestTypes!C.SetTransmitSignal r) {
-                        dev.setLoopTransmitSignal(r.buffer[0 .. numTxStream]);
-                        foreach(e; r.buffer[0 .. numTxStream]) {
-                            if(e.length) alloc.dispose(e);
-                        }
-                    },
-                    (RequestTypes!C.StartLoopTransmit r) {
-                        dev.startLoopTransmit();
-                        isStreaming = true;
-                    },
-                    (RequestTypes!C.StopLoopTransmit r) {
-                        dev.stopLoopTransmit();
-                        isStreaming = false;
-                    },
-                    (RequestTypes!C.SetParam r) {
-                        enforce(cast(IReconfigurable) dev, "The device is not IReconfigurable").setParam(r.key, r.value);
-                    },
-                    (RequestTypes!C.SyncPPS r) {
-                        if(auto syncdev = cast(IPPSSynchronizable) dev) {
-                            if(isStreaming)
-                                dev.stopLoopTransmit();
-                            
-                            syncdev = enforce(cast(IPPSSynchronizable) dev, "The device is not IPPSSynchronizable");
-                            syncdev.setTimeNextPPS(DeviceTime(0.0));
-                            syncdev.setNextCommandTime(r.settling);
-                            dev.startLoopTransmit();
-                        }
-                    }
-                );
-            }
-
-            if(isStreaming) {
-                // ループ送信に必要な処理があれば実行する
-                dev.performLoopTransmit();
-            }
-        }
-    }
+    bool _singleThread = false;
 }
+
 
 
 unittest
@@ -250,15 +313,22 @@ unittest
         void startLoopTransmit() { state = "start"; }
         void stopLoopTransmit() { state = "stop"; }
         void performLoopTransmit() { ++cntPerf; Thread.sleep(10.msecs); }
+        void setParam(const(char)[] key, const(char)[] value) {}
+        const(char)[] getParam(const(char)[] key) { return null; }
     }
 
     auto ctrl = new LoopTXController!C();
     TestDevice[] devs = [new TestDevice(2), new TestDevice(1), new TestDevice(3)];
+    foreach(d; devs) d.construct();
     ctrl.setup(devs.map!(a => cast(IDevice)a).array, null);
     ctrl.spawnDeviceThreads();
     scope(exit) ctrl.killDeviceThreads();
 
     Thread.sleep(100.msecs);
+
+    foreach(id, d; devs) {
+        assert(devs[id].state == "init");
+    }
 
     C[][] txsignals = [[C(0, 0)], [C(1, 1)], [C(2, 2)], [C(3, 3)], [C(4, 4)], [C(5, 5)]];
     
@@ -273,7 +343,7 @@ unittest
     
 
     ctrl.processMessage(txmsg, (scope const(ubyte)[] buf){});
-    ctrl.processMessage([cast(ubyte)0b0010001], (scope const(ubyte)[] buf){});
+    ctrl.processMessage([cast(ubyte)0b00010001], (scope const(ubyte)[] buf){});
     Thread.sleep(100.msecs);
 
     size_t cnt;
@@ -290,4 +360,5 @@ unittest
     ctrl.processMessage([cast(ubyte)0b0010010], (scope const(ubyte)[] buf){});
     Thread.sleep(100.msecs);
     foreach(d; devs) assert(d.state == "stop");
+    foreach(d; devs) assert(d.cntPerf > 0);
 }
