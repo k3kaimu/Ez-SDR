f8f66d6d9ba09b3162622b1de81e8591824b3595
Komatsu Kazuki (usrp-lnx01) <komatsu.kazuki.op@tut.jp>
2024-08-02 17:11:54 +0900

    add simple implementation of controller

--- source/controller/package.d
+++ source/controller/package.d
@@ -1,5 +1,7 @@
 module controller;
 
+import core.thread;
+
 import std.complex;
 import std.socket;
 import std.json;
@@ -10,11 +12,19 @@ import device;
 interface IController
 {
     void setup(IDevice[], JSONValue[string]);
+
     void spawnDeviceThreads();
     void killDeviceThreads();
 
+    void pauseDeviceThreads();
+    void resumeDeviceThreads();
+
     void processMessage(scope const(ubyte)[] msgbuf, void delegate(scope const(ubyte)[]) responseWriter);
+
+    // void applyToDeviceSync(IDevice dev, void delegate(IDevice dev));
+    // void applyToDeviceAsync(IDevice, dev, void delegate(IDevice dev));
 }
+
 
 
 IController newController(string type)
@@ -29,4 +39,214 @@ IController newController(string type)
     }
 
     // return null;
+}
+
+
+
+interface IControllerThread
+{
+    bool hasDevice(IDevice d);
+    void kill();
+    void pause();
+    void resume();
+    void callOnThis(void delegate(IControllerThread) dg);
+}
+
+
+
+class ControllerThreadImpl(DeviceType : IDevice) : Thread, IControllerThread
+{
+    import std.sumtype;
+    import msgqueue;
+    import core.sync.event;
+
+    static struct Message
+    {
+        static struct Pause {}
+        static struct CallOnThis { void delegate(IControllerThread) dg; }
+
+        alias Types = SumType!(Pause, CallOnThis);
+        alias Queue = UniqueRequestQueue!Types;
+    }
+
+
+    this()
+    {
+        _killSwitch = new shared(bool);
+        *_killSwitch = false;
+
+        _resumeEvent = new Event;
+        _resumeEvent.initialize(true, true);
+
+        _queue = new Message.Queue;
+
+        super(&run);
+    }
+
+
+    void run()
+    {
+        this.onInit();
+        _resumeEvent.wait();
+        this.onStart();
+        while(!*_killSwitch) {
+            while(!_queue.emptyRequest()) {
+                auto req = _queue.popRequest();
+                req.match!(
+                    (Message.Pause) {
+                        this.onPause();
+                        _resumeEvent.wait();
+                        this.onResume();
+                    },
+                    (Message.CallOnThis r) {
+                        r.dg(this);
+                    }
+                );
+            }
+
+            this.onRunTick();
+        }
+        this.onFinish();
+    }
+
+
+    abstract void onInit();
+    abstract void onRunTick();
+    abstract void onStart();
+    abstract void onFinish();
+    abstract void onPause();
+    abstract void onResume();
+
+
+    DeviceType[] deviceList() { return _devs; }
+
+
+    void registerDevice(DeviceType dev)
+    {
+        _devs ~= dev;
+    }
+
+
+    bool hasDevice(IDevice d)
+    {
+        foreach(e; _devs) {
+            if(e is d) return true;
+        }
+
+        return false;
+    }
+
+
+    void kill()
+    {
+        *_killSwitch = true;
+    }
+
+
+    void pause()
+    {
+        _resumeEvent.reset();
+        _queue.pushRequest(Message.Types(Message.Pause()));
+    }
+
+
+    void resume()
+    {
+        _resumeEvent.setIfInitialized();
+    }
+
+
+    void callOnThis(void delegate(IControllerThread) dg)
+    {
+        _queue.pushRequest(Message.Types(Message.CallOnThis(dg)));
+    }
+
+
+    private:
+    shared(bool)* _killSwitch;
+    Event* _resumeEvent;
+    shared(Message.Queue) _queue;
+    DeviceType[] _devs;
+}
+
+
+class ControllerImpl(CtrlThread : IControllerThread) : IController
+{
+    this() {}
+
+    abstract
+    void setup(IDevice[], JSONValue[string]);
+
+
+    void registerThread(CtrlThread thread)
+    {
+        _threads ~= thread;
+    }
+
+
+    CtrlThread[] threadList()
+    {
+        return _threads;
+    }
+
+
+    abstract
+    void spawnDeviceThreads();
+
+
+    void killDeviceThreads()
+    {
+        foreach(t; _threads) t.kill();
+    }
+
+
+    void pauseDeviceThreads()
+    {
+        foreach(t; _threads) t.pause();
+    }
+
+
+    void resumeDeviceThreads()
+    {
+        foreach(t; _threads) t.resume();
+    }
+
+
+    abstract
+    void processMessage(scope const(ubyte)[] msgbuf, void delegate(scope const(ubyte)[]) responseWriter);
+
+
+    void applyToDeviceSync(IDevice dev, void delegate() dg)
+    {
+        // 関連するすべてのDeviceThreadを一度止める
+        bool[] stopList = new bool[](_threads.length);
+        foreach(i; 0 .. _threads.length) {
+            stopList[i] = _threads[i].hasDevice(dev);
+            if(stopList[i])
+                _threads[i].pause();
+        }
+
+        // 処理を呼び出す
+        dg();
+
+        // 止めたスレッドを実行し直す
+        foreach(i; 0 .. _threads.length) {
+            if(stopList[i])
+                _threads[i].resume();
+        }
+    }
+
+
+    void applyToDeviceAsync(IDevice dev, void delegate(IControllerThread) sharedDg)
+    {
+        foreach(i; 0 .. _threads.length) {
+            if(_threads[i].hasDevice(dev)) {
+                // 一番最初に見つかったスレッドに処理を移譲する
+                _threads[i].callOnThis(sharedDg);
+            }
+        }
+    }
+
+  private:
+    CtrlThread[] _threads;
 }
